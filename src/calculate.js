55583ce53e0950f4c17147dee7892553448b5fe1
Roman Iackobchuk <kaboom.ua@gmail.com>
2015-03-23 14:22:17 +0200

    calc via regexp

--- src/calculate.js
+++ src/calculate.js
@@ -2,12 +2,14 @@
  * Created by roman on 3/23/15.
  */
 module.exports = function(exp) {
-    var result;
+    var result,
+        //reg = /[^\d()*\/+-\s]/;
+        reg = /[^\d()*/+-\s]/;
+    if (reg.test(exp)) return 'illegal exprssion';
     try {
         result = eval(exp);
     } catch (err) {
-        console.log('---', err);
-        result = "sorry, can't calculate this"
+        result = "sorry, there is an error in expression"
     }
     return result
 };
